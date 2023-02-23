b19f551425d8f2bba8a5adee774ca9985e312ddf
Tomás Caçoete <tomaspcacoete@gmail.com>
2023-02-23 12:55:49 +0000

    changed request return type top json

--- routes/entities.js
+++ routes/entities.js
@@ -3,10 +3,14 @@ const router = express.Router()
 
 //Get All entities
 router.get("/", (req, res) => {
-    res.send("Hello World")
+    res.status(200).json({
+        message: "View every entity"
+    })
 })
 //Get One entity by id
 router.get("/:id", (req, res) => {
-    res.send(req.params.id)
+    res.status(200).json({
+        ID: req.params.id
+    })
 })
 module.exports = router
